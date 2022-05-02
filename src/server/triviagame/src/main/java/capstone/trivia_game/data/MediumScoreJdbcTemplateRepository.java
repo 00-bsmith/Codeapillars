b5e32e6b3f5d3a3b64320a72283a36c362b758b7
Bryan Smith <bryans5@live.com>
2022-05-02 08:51:32 -0700

    Repo finish
    Medium and Long repos done, server boots and connects, will proceed with testing

--- src/server/triviagame/src/main/java/capstone/trivia_game/data/MediumScoreJdbcTemplateRepository.java
+++ src/server/triviagame/src/main/java/capstone/trivia_game/data/MediumScoreJdbcTemplateRepository.java
@@ -1,39 +1,94 @@
 package capstone.trivia_game.data;
 
+import capstone.trivia_game.data.mappers.ScoreMapper;
 import capstone.trivia_game.models.ScoreEntry;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
 public class MediumScoreJdbcTemplateRepository implements ScoreRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public MediumScoreJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public List<ScoreEntry> findAll() {
-        return null;
+        final String sql="select scoreId, initials, score, date from medium_high_score;";
+        return jdbcTemplate.query(sql, new ScoreMapper());
     }
 
     @Override
+    @Transactional
     public List<ScoreEntry> findByInitials(String initials) {
-        return null;
+        final String sql = "select scoreId, initials, score, date from medium_high_score " +
+                "where initials = ?;";
+
+        return jdbcTemplate.query(sql, new ScoreMapper(), initials);
     }
 
     @Override
+    @Transactional
     public ScoreEntry findById(int scoreId) {
-        return null;
+        final String sql = "select scoreId, initials, score, date from medium_high_score " +
+                "where scoreId = ?;";
+
+        ScoreEntry scoreEntry = jdbcTemplate.query(sql, new ScoreMapper(), scoreId).stream()
+                .findFirst().orElse(null);
+
+
+        return scoreEntry;
     }
 
     @Override
     public ScoreEntry add(ScoreEntry entry) {
-        return null;
+        final String sql = "insert into medium_high_score (initials, score, date) values (?, ?, ?);";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection ->{
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            //ps.setInt(1, entry.getScoreId());
+            ps.setString(1, entry.getInitials());
+            ps.setInt(2, entry.getScore());
+            ps.setTimestamp(3, Timestamp.valueOf(entry.getScoreDateTime()));
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected <=0){
+            return null;
+        }
+        entry.setScoreId(keyHolder.getKey().intValue());
+        return entry;
     }
 
     @Override
     public boolean update(ScoreEntry entry) {
-        return false;
+        final String sql = "update medium_high_score set" +
+                "initials = ?, " +
+                "score = ?, " +
+                "date=? " +
+                "where scoreId = ?;";
+
+        return jdbcTemplate.update(sql,
+                entry.getInitials(),
+                entry.getScore(),
+                Timestamp.valueOf(entry.getScoreDateTime()),
+                entry.getScoreId())>0;
     }
 
     @Override
+    @Transactional
     public boolean deleteById(int scoreId) {
-        return false;
+        return jdbcTemplate.update("delete from medium_high_score where scoreId = ?;", scoreId)>0;
     }
 }
