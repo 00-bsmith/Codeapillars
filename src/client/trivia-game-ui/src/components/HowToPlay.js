6f95e38f5abeb2aff3ffac80334ba8c7e6212e7d
srbachwich <srbachwich@gmail.com>
2022-05-04 11:41:17 -0500

    Added instructions to HowToPlay

--- src/client/trivia-game-ui/src/components/HowToPlay.js
+++ src/client/trivia-game-ui/src/components/HowToPlay.js
@@ -8,10 +8,58 @@ const HowToPlay = () => {
     <div>
       &nbsp;
     </div>
+    <img src={"images/TP-sm-med.png"} alt="TriviaPillar" />
     <div>
-      Here is how to play.....
+      Welcome, players! This is a brief guide on how to play TriviaPillar.
     </div>
-
+    <div>
+      &nbsp;
+    </div>
+    <div>
+      First, when you click the button to start a new game, you will be given a choice to play a short, medium, or long game.
+    </div>
+    <div>
+      &nbsp;
+    </div>
+    <div>
+      A short game consists of seven rounds, a medium game is fifteen rounds, and a long game has thirty rounds.
+    </div>
+    <div>
+      &nbsp;
+    </div>
+    <div>
+      During each round, you will be asked a single multiple-choice trivia question from any of a number of subjects. 
+    </div>
+    <div>
+      &nbsp;
+    </div>
+    <div>
+      You will first be given five seconds to read the question, and will not be able to answer the question during that time. 
+    </div>
+    <div>
+      &nbsp;
+    </div>
+    <div>
+      After the five seconds have elapsed, you will be given fifteen seconds to select and submit an answer to the question. 
+    </div>
+    <div>
+      &nbsp;
+    </div>
+    <div>
+      If you answer the question correctly, you will be awarded points based on how quickly you answered the question, with your maximum score being 100 points per round. If you answered incorrectly or did not submit an answer in time, you will be awarded no points.
+    </div>
+    <div>
+      &nbsp;
+    </div>
+    <div>
+      After the final round concludes, you will be shown a list of the questions you were asked, along with the correct answer as well as your final score.
+    </div>
+    <div>
+      &nbsp;
+    </div>
+    <div>
+      If you receive a high enough final score, you will be prompted to put in your initials and will be shown on our leaderboard!
+    </div>
     </>
   );
 }
