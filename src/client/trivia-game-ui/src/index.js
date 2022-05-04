8c400122e114de4e003e3f78b760e7c2676d03ab
Sheryl888 <91077647+Sheryl888@users.noreply.github.com>
2022-05-04 09:45:15 -0500

    Update index.js

--- src/client/trivia-game-ui/src/index.js
+++ src/client/trivia-game-ui/src/index.js
@@ -21,10 +21,3 @@ const root = ReactDOMClient.createRoot(container);
 // Initial render: Render an element to the root.
 root.render(<App />);
 
-const container = document.getElementById('root');
-
-// Create a root.
-const root = ReactDOMClient.createRoot(container);
-
-// Initial render: Render an element to the root.
-root.render(<App />);
