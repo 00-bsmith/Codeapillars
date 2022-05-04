26e3d1feeccdc79a55b7b3ee77a90fcdd781cf06
Sheryl888 <91077647+Sheryl888@users.noreply.github.com>
2022-05-04 12:05:34 -0500

    tweaks to Game

--- src/client/trivia-game-ui/src/components/Game.js
+++ src/client/trivia-game-ui/src/components/Game.js
@@ -42,7 +42,7 @@ const Game = () => {
     {/* once selected, have a message like: Get ready! and then the count down clock starts 3 seconds */}
 
 <div>
-<h2>Question number</h2>
+<h2>Question:</h2>
     {/* FETCH/pull in the question have it hold for a 5 seconds to allow for reading the question*/}
 
     {/* then FETCH/pull in the answer options each as buttons */}
