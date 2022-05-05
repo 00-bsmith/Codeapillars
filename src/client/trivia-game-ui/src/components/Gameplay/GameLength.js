06069efbfba55c6bc9ec2a02aaf12cfdd73eb459
srbachwich <srbachwich@gmail.com>
2022-05-05 15:03:27 -0500

    Changed submit button styling for game length

--- src/client/trivia-game-ui/src/components/Gameplay/GameLength.js
+++ src/client/trivia-game-ui/src/components/Gameplay/GameLength.js
@@ -3,7 +3,7 @@ import React from "react";
 const GameLength = () => {
   return (
     <>
-      <div>GameLength</div>
+      <h2>Game Length</h2>
 
       {/* Perhaps have a switch here for the different cases of game length to choose from and the API calls they each will make */}
 
@@ -11,20 +11,20 @@ const GameLength = () => {
         <img src={"images/TP-sm-med.png"} alt="TriviaPillar" />
         Let's Play a Game! Choose length of game:
       </div>
-      
-        <div>
-          <label htmlFor="length">Length</label>
-          <select id="length" name="length">
-            <option value="">[Select Game Length]</option>
-            <option value="chemistry">Short (7 Rounds)</option>
-            <option value="philosophy">Medium (15 Rounds)</option>
-            <option value="math">Long (30 Rounds)</option>
-          </select>
-        </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      
+      <div>&nbsp;</div>
+      <div>
+        <label htmlFor="length">Length</label>
+        <select id="length" name="length">
+          <option value="">[Select Game Length]</option>
+          <option value="short">Short (7 Rounds)</option>
+          <option value="medium">Medium (15 Rounds)</option>
+          <option value="long">Long (30 Rounds)</option>
+        </select>
+      </div>
+      <div>&nbsp;</div>
+      <button className="btn btn-success mb-3" type="submit">
+        Submit
+      </button>
 
       {/* Show options for a short (7), medium (15), or long (30) game */}
       {/* once selected, have a message like: Get ready! and then the count down clock starts 3 seconds */}
