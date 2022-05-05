495809e7475757528870d372644a03b49f1d38c4
Sheryl888 <91077647+Sheryl888@users.noreply.github.com>
2022-05-05 15:44:42 -0500

    Update GameLength.js

--- src/client/trivia-game-ui/src/components/Gameplay/GameLength.js
+++ src/client/trivia-game-ui/src/components/Gameplay/GameLength.js
@@ -14,19 +14,19 @@ const GameLength = () => {
       <div className="container">
         <div className="row">
           <div className="col">
-            <h4 className="gameLength">Short Game - 7 Questions</h4>
+            <h4 className="gameLength">Short Game - 7 Rounds</h4>
           </div>
         </div>
 
         <div className="row">
           <div className="col">
-            <h4 className="gameLength">Medium Game - 15 Questions</h4>
+            <h4 className="gameLength">Medium Game - 15 Rounds</h4>
           </div>
         </div>
 
         <div className="row">
           <div className="col">
-            <h4 className="gameLength">Long Game - 30 Questions</h4>
+            <h4 className="gameLength">Long Game - 30 Rounds</h4>
           </div>
         </div>
 
