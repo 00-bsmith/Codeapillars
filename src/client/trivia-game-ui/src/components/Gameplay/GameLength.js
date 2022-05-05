50a9b66d45c763d06c799f8589a1e11aa6dd013c
srbachwich <srbachwich@gmail.com>
2022-05-05 15:28:12 -0500

    Beautified GameLength code

--- src/client/trivia-game-ui/src/components/Gameplay/GameLength.js
+++ src/client/trivia-game-ui/src/components/Gameplay/GameLength.js
@@ -1,61 +1,68 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-  
+import React from "react";
+import { Link } from "react-router-dom";
+
 const GameLength = () => {
   return (
     <>
-    <div>     
-    <h1><img src={"images/TP-sm-med.png"} alt="TriviaPillar" />
-      Let's Play a Game! Choose length of game:</h1>
-    </div>
-
-    <div className="container">
-      <div className="row">
-          <div className="col"><h4 className='gameLength'>Short Game - 7 Questions</h4></div>
+      <div>
+        <h1>
+          <img src={"images/TP-sm-med.png"} alt="TriviaPillar" />
+          Let's Play a Game! Choose length of game:
+        </h1>
       </div>
 
-      <div className="row">
-        <div className="col">
-          <h4 className='gameLength'>Medium Game - 15 Questions</h4>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h4 className="gameLength">Short Game - 7 Questions</h4>
+          </div>
         </div>
-      </div>
 
-      <div className="row">
-        <div className="col">
-          <h4 className='gameLength'>Long Game - 30 Questions</h4>
+        <div className="row">
+          <div className="col">
+            <h4 className="gameLength">Medium Game - 15 Questions</h4>
+          </div>
         </div>
-      </div>
 
+        <div className="row">
+          <div className="col">
+            <h4 className="gameLength">Long Game - 30 Questions</h4>
+          </div>
+        </div>
 
-
-      <div className='row'>
+        <div className="row">
           <label htmlFor="length">Length</label>
           <select id="length" name="length">
             <option value="">[Select Game Length]</option>
-            <option value="chemistry">Short (7 Rounds)</option>
-            <option value="philosophy">Medium (15 Rounds)</option>
-            <option value="math">Long (30 Rounds)</option>
+            <option value="short">Short (7 Rounds)</option>
+            <option value="medium">Medium (15 Rounds)</option>
+            <option value="long">Long (30 Rounds)</option>
           </select>
         </div>
 
         {/* <div>
           <button type="submit">Submit</button>
         </div> */}
-      
-      <br />
-      <br />
-      <br />
-      <br />
+
+        <br />
+        <br />
+        <br />
+        <br />
 
         <div className="row">
           <div className="col">
-            <Link to="/game" className="btn btn-success mb-3 ml-2" type="submit">Start Game</Link>
+            <Link
+              to="/game"
+              className="btn btn-success mb-3 ml-2"
+              type="submit"
+            >
+              Start Game
+            </Link>
           </div>
         </div>
+      </div>
 
-    </div>
-      
-        {/* <img src={"images/TP-sm-med.png"} alt="TriviaPillar" />
+      {/* <img src={"images/TP-sm-med.png"} alt="TriviaPillar" />
         Let's Play a Game! Choose length of game:
       </div>
       <div>&nbsp;</div>
