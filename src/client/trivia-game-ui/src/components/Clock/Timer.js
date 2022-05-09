79e9553e869849462c7abba40677ad209bbc6c06
srbachwich <srbachwich@gmail.com>
2022-05-09 11:43:26 -0500

    Added conditions to stopTimer() and calculateScore() in Timer

--- src/client/trivia-game-ui/src/components/Clock/Timer.js
+++ src/client/trivia-game-ui/src/components/Clock/Timer.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import { useState } from 'react';
-import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+import React from "react";
+import { useState } from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import "./styles.css";
-import Round from '../Gameplay/Round';
-import Score from '../Gameplay/Score';
-
+import Round from "../Gameplay/Round";
+import Score from "../Gameplay/Score";
 
 function Timer(props) {
   const [currentTime, setCurrentTime] = React.useState(0);
@@ -26,218 +25,218 @@ function Timer(props) {
   const [duration, setDuration] = useState(5);
   const [remainingTime, setRemainingTime] = useState(duration);
 
-console.log("Round: " + round);
-console.log("Duration: " + duration);
+  console.log("Round: " + round);
+  console.log("Duration: " + duration);
 
-const stopTimer = () => {
-  setSeconds(currentTime);
-  setIsPlaying(false);
-  calculateScore();
+  const stopTimer = () => {
+    setSeconds(currentTime);
+    // setIsPlaying(false);
+    if (isPlaying == true) {
+      calculateScore();
+    }
+    setIsPlaying(false);
     console.log("secs: " + currentTime);
     console.log("dur: " + duration);
     console.log("Time Stopped @: " + currentTime);
   };
 
   const calculateScore = () => {
-    const time = duration - currentTime;
-    const a = time / duration;
+    if (duration === 15) {
+      const time = duration - currentTime;
+      const a = time / duration;
       console.log("a: " + a);
-    const b = a / 1.2;
+      const b = a / 1.2;
       console.log("b: " + b);
-    const c = 1 - b;
+      const c = 1 - b;
       console.log("c: " + c);
-    const d = c * 100;
+      const d = c * 100;
       console.log("d: " + d);
-    const score = d;
+      const score = d;
       console.log("a: " + a);
-    setScore(Math.round(score));
-    props.getScore(Math.round(score));
-    };
+      setScore(Math.round(score));
+      props.getScore(Math.round(score));
+    }
+  };
 
+  // When the timer runs out, the score is 17. It should be zero
+  //if current time === 0 then score should 0 now 17.
 
+  // const zeroScore = () => {
+  //   if (remainingTime === 0 && currentTime ===0) {
+  //     const z = ({score} - 17);
+  //     const zero = z;
+  //     setZero(0);
+  //   }
+  // }
 
-    // When the timer runs out, the score is 17. It should be zero
-    //if current time === 0 then score should 0 now 17.
-
-    // const zeroScore = () => {
-    //   if (remainingTime === 0 && currentTime ===0) {
-    //     const z = ({score} - 17);
-    //     const zero = z;
-    //     setZero(0);
-    //   }
-    // }
-
-  
-    // probably wont need this restart functionality here. Perhaps on the results page to get next question?
-    const restartTimer = () => {
+  // probably wont need this restart functionality here. Perhaps on the results page to get next question?
+  const restartTimer = () => {
     setCurrentTime(seconds);
     setIsPlaying(true);
-    };
-    
-    // not sure about this reset. Currently it isn't working here. 
-    const resetTimer = () => {
+  };
+
+  // not sure about this reset. Currently it isn't working here.
+  const resetTimer = () => {
     setIsPlaying(true);
     setDuration(15);
-    };
+  };
 
-    
+  const renderTime = ({ remainingTime }) => {
+    setCurrentTime(remainingTime);
+    if (remainingTime === 0) {
+      return <div className="timer">Time is Up!</div>;
+    }
+    if (duration === 5) {
+      return (
+        <div className="timer">
+          <div className="text">Read Question</div>
+          <div className="value">{remainingTime}</div>
+          <div className="text">seconds</div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="timer">
+          <div className="text">Choose Answer</div>
+          <div className="value">{remainingTime}</div>
+          <div className="text">seconds</div>
+        </div>
+      );
+    }
+  };
 
-    const renderTime = ({ remainingTime }) => {
-      setCurrentTime(remainingTime);
-      if (remainingTime === 0) {
-        return <div className="timer">Time is Up!</div>;
-      }  
-      if (duration === 5) {
-        return (
-          <div className="timer">
-            <div className="text">Read Question</div>
-            <div className="value">{remainingTime}</div>
-            <div className="text">seconds</div>
-          </div>
-          );  
-      } else {
-        return (
-          <div className="timer">
-            <div className="text">Choose Answer</div>
-            <div className="value">{remainingTime}</div>
-            <div className="text">seconds</div>
-          </div>
-          );
-        }
-    };
+  // Open the console to check the value for currentTime
+  console.log(currentTime);
 
-                     // Open the console to check the value for currentTime
-                     console.log(currentTime);
+  // ////////////////Trying to set this up to share the round with Round.js//////////
 
-// ////////////////Trying to set this up to share the round with Round.js//////////
+  // const getRound = (newRound) => {
+  //   props.getRound;
+  // }
 
-// const getRound = (newRound) => {
-//   props.getRound;
-// }
+  // class Timer extends React.Component {
+  //     constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //         data: {round}
+  //       }
+  //     }
+  //     render() {
+  //       return (
 
+  //          <div>
+  //             <div>{this.state.round}</div>
+  //              <Round value={this.state.round} />
+  //             </div>
+  //       );
+  //     }
+  // }
+  // ///////////////////////////////////////////////////////////////
 
+  return (
+    <>
+      <div className="timer-wrapper">
+        <CountdownCircleTimer
+          key={key}
+          isPlaying={isPlaying}
+          duration={duration} // duration is a number i.e. duration={5}
+          colors={["#0a9396", "#ffb703", "#f77f00", "#ae2012"]}
+          colorsTime={[5, 3, 1, 0]}
+          //  original parameter:   onComplete={() => ({ shouldRepeat: true, delay: 0 })}
 
-// class Timer extends React.Component {
-//     constructor(props) {
-//     super(props);
-//     this.state = {
-//         data: {round}
-//       }
-//     }
-//     render() {
-//       return (
-                  
-//          <div>
-//             <div>{this.state.round}</div>
-//              <Round value={this.state.round} />
-//             </div>
-//       );
-//     }
-// }                 
-// ///////////////////////////////////////////////////////////////
- 
- return (
- <>
-
-  <div className="timer-wrapper">
- 
-    <CountdownCircleTimer
-    key={key}
-    isPlaying={isPlaying}
-    duration={duration}  // durationn is a number i.e. duration={5}
-    colors={["#0a9396", "#ffb703", "#f77f00", "#ae2012"]}
-    colorsTime={[5, 3, 1, 0]}
-    
-    //  original parameter:   onComplete={() => ({ shouldRepeat: true, delay: 0 })}
-
-    onComplete={() => {
-      if(round === 3 && duration === 15) {
-        // counter is the number of rounds this needs to be dynamic!!!
-        if(round === 3 && duration === 15) {
-          setRemainingTime(0);
-          setIsPlaying(false);
-          // setZero(0);
-        } else {
-          if (duration === 5) {
-            setDuration(15);
-          } else {
-            setRound(round+1);
-            setDuration(5);
-          }
-        setKey((prevKey) => prevKey + 1);
-        }
-        
-      } else if (difficulty === "Medium") {
-        if(round === 3 && duration === 15) {
-          setRemainingTime(0);
-          setIsPlaying(false);
-          // setZero(0);
-        } else {
-          if (duration === 5) {
-            setDuration(15);
-          } else {
-            setRound(round+1);
-            setDuration(5);
-          }
-        setKey((prevKey) => prevKey + 1);
-        }
-
-      } else if (difficulty === "Hard") {
-        if(round === 3 && duration === 15) {
-          setRemainingTime(0);
-          setIsPlaying(false);
-          // setZero(0);
-        } else {
-            if (duration === 5) {
-              setDuration(15);
-            } else {
-              setRound(round+1);
-              setDuration(5);
+          onComplete={() => {
+            if (round === 3 && duration === 15) {
+              // counter is the number of rounds this needs to be dynamic!!!
+              if (round === 3 && duration === 15) {
+                setRemainingTime(0);
+                setIsPlaying(false);
+                // setZero(0);
+              } else {
+                if (duration === 5) {
+                  setDuration(15);
+                } else {
+                  setRound(round + 1);
+                  setDuration(5);
+                }
+                setKey((prevKey) => prevKey + 1);
+              }
+            } else if (difficulty === "Medium") {
+              if (round === 3 && duration === 15) {
+                setRemainingTime(0);
+                setIsPlaying(false);
+                // setZero(0);
+              } else {
+                if (duration === 5) {
+                  setDuration(15);
+                } else {
+                  setRound(round + 1);
+                  setDuration(5);
+                }
+                setKey((prevKey) => prevKey + 1);
+              }
+            } else if (difficulty === "Hard") {
+              if (round === 3 && duration === 15) {
+                setRemainingTime(0);
+                setIsPlaying(false);
+                // setZero(0);
+              } else {
+                if (duration === 5) {
+                  setDuration(15);
+                } else {
+                  setRound(round + 1);
+                  setDuration(5);
+                }
+                setKey((prevKey) => prevKey + 1);
+              }
             }
-          setKey((prevKey) => prevKey + 1);
-        }
-      }  
 
-      return { shouldRepeat:true, delay: 1.5 }
-    }}
-  >
-    {/* {({ remainingTime }) => remainingTime} */}
-    
-    {renderTime}
-  </CountdownCircleTimer>
-</div>
+            return { shouldRepeat: true, delay: 1.5 };
+          }}
+        >
+          {/* {({ remainingTime }) => remainingTime} */}
 
-{/* GRAB round and pass to Round.js to display */}
-{/* <p id="round">
+          {renderTime}
+        </CountdownCircleTimer>
+      </div>
+
+      {/* GRAB round and pass to Round.js to display */}
+      {/* <p id="round">
 {currentTime}   Current time (PRINTED OUT) 
 {setCurrentTime}
 </p> */}
 
-<div className="round-container">
+      <div className="round-container">
+        {/* This stop-timer-button needs to ALSO submit answer !!!*/}
+        <button
+          type="submit"
+          className="btn btn-primary stop-timer-button"
+          onClick={stopTimer}
+        >
+          Submit
+        </button>
+        {/* probably dont need a restart button, just here to play with for now */}
+        <button
+          type="submit"
+          className="btn btn-success stop-button"
+          onClick={restartTimer}
+        >
+          restart
+        </button>
 
-{/* This stop-timer-button needs to ALSO submit answer !!!*/}
-<button type="submit" className="btn btn-primary stop-timer-button" onClick={stopTimer}>Submit</button>
-{/* probably dont need a restart button, just here to play with for now */}
-<button type="submit" className="btn btn-success stop-button" onClick={restartTimer}>restart</button>
-
-{/* Dont need this bit for functionality, it's just displayed to vew functionality for now.*/}
-  <p id="round">
-    <br />
-      Round: {round} (PRINTED OUT)
-    <br />
-      Duration Time: {duration - seconds}  (PRINTED OUT)
-    <br />
-      isPlaying: {isPlaying.toString()} (PRINTED OUT)
-    <br />
-      SCORE: {score}  (PRINTED OUT)
-  </p>
-
-</div>
-
-</>
-);
-      
+        {/* Dont need this bit for functionality, it's just displayed to vew functionality for now.*/}
+        <p id="round">
+          <br />
+          Round: {round} (PRINTED OUT)
+          <br />
+          Duration Time: {duration - seconds} (PRINTED OUT)
+          <br />
+          isPlaying: {isPlaying.toString()} (PRINTED OUT)
+          <br />
+          SCORE: {score} (PRINTED OUT)
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default Timer;
-
