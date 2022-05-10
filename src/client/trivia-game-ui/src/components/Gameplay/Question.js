bd4da0e0539eb5ac520d9fc9608a6ca2713e7333
srbachwich <srbachwich@gmail.com>
2022-05-10 16:21:47 -0500

    Added shuffling of answers and basic radio buttons
    Radio buttons need fixing, multiple can be selected at once

--- src/client/trivia-game-ui/src/components/Gameplay/Question.js
+++ src/client/trivia-game-ui/src/components/Gameplay/Question.js
@@ -4,12 +4,15 @@ export const Question = (props) => {
   const [question, setQuestion] = useState([]);
   const [questionTitle, setQuestionTitle] = useState("");
   const [answers, setAnswers] = useState([]);
+  const [shuffledAnswers, setShuffledAnswers] = useState([]);
   const [correctAnswer, setCorrectAnswer] = useState("");
   //   const [otherAnswers, setOtherAnswers] = useState([]);
   const [pointValue, setPointValue] = useState(0);
   const [answered, setAnswered] = useState(false);
   const [correct, setCorrect] = useState(false);
   const [earnedPoints, setEarnedPoints] = useState(0);
+
+
 
   const [answer, setAnswer] = useState("");
 
@@ -36,7 +39,30 @@ export const Question = (props) => {
 
   useEffect(() => {
     console.log(correctAnswer);
+    console.log(answers);
+    shuffle(answers);
+    setShuffledAnswers(answers);
+    console.log(answers);
+    console.log(correctAnswer);
   }, [correctAnswer]);
+
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
 
   return (
     <>
@@ -45,32 +71,15 @@ export const Question = (props) => {
       </div>
 
       <div>
-          
+          <form className="answers">
+              <label><input type="radio" value={shuffledAnswers[0]} name="1"/>{shuffledAnswers[0]}</label>
+              <label><input type="radio" value={shuffledAnswers[1]} name="2"/>{shuffledAnswers[1]}</label>
+              <label><input type="radio" value={shuffledAnswers[2]} name="3"/>{shuffledAnswers[2]}</label>
+              <label><input type="radio" value={shuffledAnswers[3]} name="4"/>{shuffledAnswers[3]}</label>
+          </form>
       </div>
     </>
   );
 };
 
 export default Question;
-// this is code from the quizbee app. The app had both question and the answer options displaying together. This is here for reference. Perhaps we can pull out what we need for the answer options and build up the AnswerOptions.js
-
-// const Question = ({question, options, selected}) => {
-//   const [answer, setAnswer] = useState(options);
-//   return (
-//     <div className="question">
-//       <div className="question">{question}</div>
-//       {answer.map((text, index) => (
-//         <button
-//           key={index}
-//           className="answerBtn"
-//           onClick={() => {
-//             setAnswer([text]);
-//             selected(text);
-//           }}
-//         >
-//           {text}
-//         </button>
-//       ))}
-//     </div>
-//   );
-// };
